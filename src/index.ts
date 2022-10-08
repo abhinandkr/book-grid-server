f52fe5decf1e39f21dfc06bd008072671df262ea
abhinandkr <abhinandkr1@gmail.com>
2022-10-08 12:35:07 +0530

    Deploying on AWS
    * Added AWS CodeBuild support
    * Setup AWS ElasticBeanstalk

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import './pre-start'; // Must be the first import
 import logger from 'jet-logger';
 import server from './server';
-
+import {getEnv} from '@shared/functions';
 
 // Constants
 const serverStartMsg = 'Express server started on port: ';
-const port = (process.env.PORT || 4000);
+const port = (parseInt(getEnv('PORT')!) || 4000);
 
 // Start server
 server.listen(port, () => {
